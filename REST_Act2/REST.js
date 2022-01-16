1d1eafd3b920507219c0eb97b54effe693e5aecc
TiagoRomao-MEIW <92744449+TiagoRomao-MEIW@users.noreply.github.com>
2022-01-16 15:42:38 +0000

    v4 - update final, relatório

--- REST_Act2/REST.js
+++ REST_Act2/REST.js
@@ -1,40 +1,26 @@
-async function getREST() {
 
+async function getREST() {
+    //const URL = 'http://ken.utad.pt:8181/check/';
+
+    // utilização de um proxy CORS Policy
     const URL = 'https://cors-anywhere.herokuapp.com/http://ken.utad.pt:8181/check/';
     const NumConta = '12345678';
     const Valor = '1000';
     const response = await fetch(URL + NumConta + '/amount/' + Valor);
-
     const data = await response.json();
-    const {
-        date,
-        checkID
-    } = data;
+    const {date, checkID} = data;
 
-    document.getElementById('GetDate').textContent = date;
+    document.getElementById('pResultado').textContent =
+        'Resultado do método Get para a conta ' + NumConta + ' e valor ' + Valor;
+    document.getElementById('GetDate').textContent = date;1
     document.getElementById('GetCheckID').textContent = checkID;
-    /* 
-        //testes
-        console.log(data.date, ', vindo do data.');
-        console.log(data.checkID, ', vindo do data.');
-        console.log(date, ', vindo da const');
-        console.log(checkID, ', vindo da const');
-     */
+
+    //testes
+    //console.log(data);
+    console.log('Número de conta: ', NumConta);
+    console.log('Valor do cheque: ', Valor);
+    console.log('Resultado | Data: ', data.date);
+    console.log('Resultado | Número do cheque: ', data.checkID); 
 }
+
 getREST();
-
-
-/* // XMLHttpRequest
-const URL = 'https://cors-anywhere.herokuapp.com/http://ken.utad.pt:8181/check/12345678/amount/1000';
-const request = new XMLHttpRequest();
-request.open ('GET', URL);
-request.send();
-request.onload = ()=>{
-    if(request.status === 200){
-        
-        console.log(JSON.parse(request.responseText));
-    }
-    else{
-        console.log('error ${request.status}')
-    }
-}  */
